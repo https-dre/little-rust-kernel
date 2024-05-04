f0f74a0a07a4b8b7e44f418d4ee37da8470fe758
https-dre <diaso.andre@outlook.com>
2024-05-04 14:39:35 -0300

    feat: add the new_line and the clear_row methods now we are using the "volatile" lib

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,5 @@
+use volatile::Volatile;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -42,7 +44,7 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT], // uma matriz bidimensional
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT], // uma matriz bidimensional
     // [type, size]
 }
 
@@ -65,17 +67,35 @@ impl Writer {
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
-                    color_code,
-                };
+                    color_code
+                });
                 self.column_position += 1;
             }
         }
     }
 
     fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read(); // pega o caracter
+                self.buffer.chars[row - 1][col].write(character);    // move para cima
+            }
+        }
 
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 2;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
     }
 
     pub fn write_string(&mut self, s: &str) {
@@ -90,13 +110,23 @@ impl Writer {
     }
 }
 
+use core::fmt;
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 pub fn print_something() {
+    use core::fmt::Write;
     let mut writer = Writer {
-        column_position: 0,
+        column_position: 2,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer)},
     };
 
-    writer.write_string("Prompt of my Operating System")
-
+    write!(writer, "My own Operating System written in Rust\n").unwrap();
+    write!(writer, "Testing New Lines Method").unwrap();
 }
