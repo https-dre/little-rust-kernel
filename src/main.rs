8f421049dc5a724a3fbb6734426dae3fc21163ed
https-dre <diaso.andre@outlook.com>
2024-05-06 20:06:02 -0300

    add one test into main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
@@ -13,9 +17,13 @@ pub extern "C" fn _start() -> ! {
     write!(vga_buffer::WRITER.lock(), "My Own Operating System Written In Rust\n").unwrap();
     println!("Hello World!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
